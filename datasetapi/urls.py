fca0c28bee4a010e10d705d90f800401d5184b6f
dipakgupta12 <dipakgupta12121@gmail.com>
2022-02-22 19:16:00 +0530

    some changes

--- datasetapi/urls.py
+++ datasetapi/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 from rest_framework import routers
 from csvfile.views import DataViewSet
@@ -25,6 +26,7 @@ router.register('data', DataViewSet)
 urlpatterns = router.urls
 
 urlpatterns += [
+    path('', RedirectView.as_view(url='data/')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
